a1c503dc10fa66540d94e6b8a2dfb78020230d65
Louis Shi <louis.shi@uconn.edu>
2018-01-04 04:25:12 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,6 +19,6 @@ function dec(n){
 function makeInt(n){
   return parseInt('n', 10)
 }
-function makeInt(nonsense){
-  return parseInt("nonsense!", 953)
+function makeInt(Nan){
+  return parseInt('Nan', 953)
 }
